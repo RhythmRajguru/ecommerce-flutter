ed8ee2b004a3775ba8bafa6fe25a390f7a26f630
Rhythm <rajgururhythm493@gmail.com>
2025-04-09 19:13:40 +0530

    constants created

---
+++ lib/utils/constants/app_constraint.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+
+class AppConstant{
+  AppConstant._();
+
+  static String appMainName='Ecomm';
+  static String appPoweredBy='Powered By Rhythm';
+  static const appMainColor=Color(0xFFbf1b08);
+  static const appSecondaryColor=Color(0xFF981206);
+  static const appTextColor=Color(0xFFFBF5F4);
+  static const appStatusBarColor=Color(0xFFFBF5F4);
+}
