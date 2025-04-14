4d8c57eac0e0c1cb63ed9a605762c8a7907f3a61
Rhythm <rajgururhythm493@gmail.com>
2025-04-14 12:22:27 +0530

    product incresing and descring btn working done in cart screen

--- lib/screens/user-panel/cart_screen.dart
+++ lib/screens/user-panel/cart_screen.dart
@@ -7,6 +7,7 @@ import 'package:ecom/utils/constants/app_constraint.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_swipe_action_cell/core/cell.dart';
 import 'package:get/get.dart';
 import 'package:image_card/image_card.dart';
 
@@ -22,8 +23,8 @@ class CartScreen extends StatelessWidget {
         title: Text('Cart',style: TextStyle(color: AppConstant.appTextColor),),
         iconTheme: IconThemeData(color: AppConstant.appTextColor),
       ),
-       body: FutureBuilder(
-         future: FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').get(),
+       body: StreamBuilder(
+         stream: FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').snapshots(),
          builder: (BuildContext context,AsyncSnapshot<QuerySnapshot> snapshot) {
            if(snapshot.hasError){
              return Center(child: Text('Error'),);
@@ -62,27 +63,59 @@ class CartScreen extends StatelessWidget {
                         updatedAt: cartData['updatedAt'],
                         productQuantity: cartData['productQuantity'],
                         productTotalPrice: cartData['productTotalPrice']);
-                  return Card(
-                    color: AppConstant.appTextColor,
-                    elevation: 5,
-                    child: ListTile(
-                      leading: CircleAvatar(backgroundColor: AppConstant.appMainColor,backgroundImage: NetworkImage(cartModel.productImages[0],),),
-                      title: Text(cartModel.productName),
-                      subtitle: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(cartModel.productTotalPrice.toString()),
-                         Row(
-                           children: [
-                             CircleAvatar(radius: 14.0,backgroundColor: AppConstant.appMainColor,child: Icon(Icons.remove,color: AppConstant.appTextColor,),),
-                             SizedBox(width: 10,),
-                             CircleAvatar(radius: 14.0,backgroundColor: AppConstant.appMainColor,child: Icon(Icons.add,color: AppConstant.appTextColor,),),
-                           ],
-                         )
-                        ],
-                      ),
-                    ),
-                  );
+                  return SwipeActionCell
+                    (key: ObjectKey(cartModel.productId),
+                      trailingActions: [
+                        SwipeAction(
+                          title: 'Delete',
+                            forceAlignmentToBoundary: true,
+                            performsFirstActionWithFullSwipe: true,
+                            onTap: (CompletionHandler handler)async{
+                            await FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').doc(cartModel.productId).delete();
+                            })
+                      ],
+                      child: Card(
+                        color: AppConstant.appTextColor,
+                        elevation: 5,
+                        child: ListTile(
+                          leading: CircleAvatar(backgroundColor: AppConstant.appMainColor,backgroundImage: NetworkImage(cartModel.productImages[0],),),
+                          title: Text(cartModel.productName),
+                          subtitle: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text(cartModel.productTotalPrice.toString()),
+                              Row(
+                                children: [
+                                  InkWell(
+                                    onTap: (){
+                                      if(cartModel.productQuantity>1){
+                                        FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').doc(cartModel.productId).update({
+                                          'productQuantity':cartModel.productQuantity-1,
+                                          'productTotalPrice':cartModel.productTotalPrice-double.parse(cartModel.isSale?cartModel.salePrice:cartModel.fullPrice)
+                                        });
+                                      }else{
+                                        if(cartModel.productQuantity==1){
+                                          FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').doc(cartModel.productId).delete();
+                                        }
+                                      }
+                                    },
+                                      child: CircleAvatar(radius: 14.0,backgroundColor: AppConstant.appMainColor,child: Icon(Icons.remove,color: AppConstant.appTextColor,),)),
+                                  SizedBox(width: 10,),
+                                  InkWell(
+                                      onTap: (){
+                                        FirebaseFirestore.instance.collection('cart').doc(user!.uid).collection('cartOrders').doc(cartModel.productId).update({
+                                          'productQuantity':cartModel.productQuantity+1,
+                                          'productTotalPrice':cartModel.productTotalPrice+double.parse(cartModel.isSale?cartModel.salePrice:cartModel.fullPrice)
+                                        });
+
+                                      },
+                                      child: CircleAvatar(radius: 14.0,backgroundColor: AppConstant.appMainColor,child: Icon(Icons.add,color: AppConstant.appTextColor,),)),
+                                ],
+                              )
+                            ],
+                          ),
+                        ),
+                      ),);
                 },)
 
               );
